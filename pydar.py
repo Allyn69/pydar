871cfd13aeccabc8f610bc31743e03b7fe2b1156
Peter Pagac <QuarKUS7@gmail.com>
2019-01-24 11:22:17 +0100

    redundant imports fixed

--- pydar.py
+++ pydar.py
@@ -1,16 +1,8 @@
-import random
-import argparse
-import requests
 import matplotlib.pyplot as plt
 from matplotlib import animation
-import cartopy.crs as ccrs
-from cartopy.io.img_tiles import OSM, GoogleTiles, Stamen
-from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
-from geopy import Point
-from geopy.distance import vincenty
-import numpy as np
-import random
-import cartopy.feature as cpf
+from cartopy.io.img_tiles import OSM
+
+#Local aplication import
 from pydar.pydar import create_map, update_flights, create_extent, parse_args
 
 if __name__ == '__main__':
@@ -24,16 +16,10 @@ if __name__ == '__main__':
     EXTENT = create_extent(LONGITUDE, LATITUDE, DISTKM)
     figure, axes, track_flights = create_map(LONGITUDE, LATITUDE, EXTENT)
 
-    # Create empty global variables for update_flights function
-    flight_list = {}
-    coords_list = []
-    color_list = []
-    annotation_list = []
-
     # Update the plot every 2 seconds until close
     #TODO: use blit to speed up
-    anim = animation.FuncAnimation(figure, update_flights, fargs=[LONGITUDE, LATITUDE, DISTKM, flight_list, figure, axes, track_flights],
-                                   interval=2000, blit=False)
+    anim = animation.FuncAnimation(figure, update_flights, fargs=[LONGITUDE, LATITUDE, DISTKM, figure, axes, track_flights], interval=2000, blit=False)
+
     # Incase of missing tiles try to fetch them again
     try:
         plt.show()
